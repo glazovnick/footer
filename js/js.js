f42c08276e1f547729dc4b3a446bb97051e7a3e6
glazovnick <glazovnick@gmail.com>
2017-08-22 12:41:57 +0300

    start

---
+++ js/js.js
@@ -0,0 +1,36 @@
+! function ($) {
+
+	//Map Google
+	    /*function init_map()
+	    {
+		var map_canvas = document.getElementById('contacts-map');
+		var map_options = {
+			center: new google.maps.LatLng(55.749585, 37.5372791),
+			zoom: 16,
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			mapTypeControlOptions: {
+				style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+				position: google.maps.ControlPosition.BOTTOM_RIGHT
+			},
+			zoomControl: true,
+			zoomControlOptions: {
+				style: google.maps.ZoomControlStyle.SMALL,
+				position: google.maps.ControlPosition.LEFT_CENTER
+			},
+			panControl: false
+		}
+		var map = new google.maps.Map(map_canvas, map_options)
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(55.749585, 37.5372791),
+			map: map,
+			icon: 'img/float.png'
+		});
+
+	}
+
+	if($('#contacts-map').length > 0)
+	{
+		google.maps.event.addDomListener(window, 'load', init_map);
+	}*/
+	
+}(window.jQuery);
